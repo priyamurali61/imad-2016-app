51cc839e65a35687cbbb6bfa584c7d1bdaee945e
priyamurali61 <priyamurali61@gmail.com>
2016-11-06 12:16:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+counter=0;
+button.onClick = function(){
+   counter=counter+1;
+   var span=document.getElementById('count');
+   span.innerHTML= counter.toString();
+    
+}
