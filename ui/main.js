dc44617d0e367a6749647b88be2aaf66e4e4f7b2
priyamurali61 <priyamurali61@gmail.com>
2016-11-06 19:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onClick = function(){
   
     
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick = function() {
      var request = new XMLHttpRequest();
@@ -40,6 +39,8 @@ submit.onclick = function() {
             
     }
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
  request.open('GET','http://priyamurali61.imad.hasura-app.io/submit=name?name=' + name, true);
    request.send(null);
 };
