f95fb1b4c980136ba9c86fcabaae1a4bdc89d40a
priyamurali61 <priyamurali61@gmail.com>
2016-11-07 11:52:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input , salt,10000,S12 ,'shaS12');
-    return hashed.toString('hex');
+    return ["pbkdf2" , "10000" ,salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
    var hashedString =hash(req.params.input,'this-is-some-random-string') ;
